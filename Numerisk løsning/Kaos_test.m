748eb4d96c826f4a66b4b0da0965961aef219c22
nadiibea <biuta12345@gmail.com>
2026-04-10 12:47:22 +0200

    Delete Numerisk løsning/Kaos_test.m

--- Numerisk løsning/Kaos_test.m
+++
@@ -1,8 +0,0 @@
-% Definer filnavnet (vigtigt med ' ' omkring pga. mellemrum)
-filename = '50 Hz - 1,5 t.csv';
-
-% Læs filen ind som en tabel
-data = readtable(filename);
-
-% Vis de første par rækker i kommandovinduet
-head(data)
